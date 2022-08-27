d53a7ea314e36077b52fd77927af56685bd45ed6
loukylor <loukylor@gmail.com>
2022-08-27 02:12:23 -0700

    remove unessecary arg

--- main.py
+++ main.py
@@ -3,14 +3,13 @@ import os
 from flask import Flask
 from flask_restful import Api
 from pymongo import MongoClient
-from pymongo.server_api import ServerApi
 
 app = Flask(__name__)
 api = Api(app)
 
 load_dotenv()
 
-mongo_client = MongoClient(os.environ["MONGODB_URL"], server_api=ServerApi('1'))
+mongo_client = MongoClient(os.environ["MONGODB_URL"])
 db = mongo_client["EduHack"]
 user_credentials = db["UserCredentials"]
 
